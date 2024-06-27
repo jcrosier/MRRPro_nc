648251863c223e233f2c1ab1c3bd7bc1b1c35e2f
jonny <j.crosier@manchester.ac.uk>
2024-06-27 14:12:54 +0100

    update list of examples

--- mrr_examples.py
+++ mrr_examples.py
@@ -3,19 +3,16 @@ import mrr_finder
 import mrr_nc_merger
 
 # Example: Get List of all MRR files it a given folder
-#file_list = mrr_finder.files_in_path('./data/2023/202303/20230322/')
+file_list = mrr_finder.files_in_path('./data/2023/202303/20230322/')
 
 # Example: Plot image of reflectivity vs time, range
-#mrr_plots.plot_2d_data(file_list, 'Ze', [True, -10, 30], offset_y=True)
+mrr_plots.plot_2d_data(file_list, 'Ze', [True, -10, 30], offset_y=True)
 
 # Example: Merge all MRR nc files in a given folder
-#result = mrr_nc_merger.mrr_merge_folder('./data/2023/202303/20230322/')
+result = mrr_nc_merger.mrr_merge_folder('./data/2023/202303/20230322/')
 
 # Example: Get a list of all sub-folders containing MRR files (e.g. all days in March 2023)
-#folder_list = mrr_finder.folders_in_path('./data/2023/202303/')
+folder_list = mrr_finder.folders_in_path('./data/2023/202303/')
 
 # Example: Multithread merge all MRR nc files for each sub-folder in a list of folders
-#list_result = mrr_nc_merger.mrr_merge_list(folder_list, n_cpu=6)
-
-result = mrr_nc_merger.mrr_merge_folder('C:/Users/jonny/Desktop/temp mrr/20240522/')
-mrr_plots.plot_2d_data(['C:/Users/jonny/Desktop/temp mrr/20240522/MRRPro__0.nc'], 'Ze', [True, -10, 30], offset_y=True)
+list_result = mrr_nc_merger.mrr_merge_list(folder_list, n_cpu=6)
